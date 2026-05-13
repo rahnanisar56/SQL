e089ae8108f05c49a5352df47202e5ffe816b01d
rahnanisar <rahnanisar56@gmail.com>
2026-05-13 10:05:04 +0530

    cw day 8

---
+++ cw8.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE groceryshop;
+USE groceryshop;
+CREATE TABLE products(product_id int(100),product_name varchar(25),price int(100));
+ALTER TABLE products ADD category int(10);
+TRUNCATE TABLE products;
